a8a2b455f8734c06278432aad97218d5f6840ccf
rcaa <rodrigo.andrade@ufape.edu.br>
2022-03-04 15:01:47 -0300

    criacao do model Endereco

---
+++ db/migrate/20220304180118_create_enderecos.rb
@@ -0,0 +1,13 @@
+class CreateEnderecos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :enderecos do |t|
+      t.string :logradouro
+      t.string :complemento
+      t.string :cep
+      t.string :bairro
+      t.string :cidade
+
+      t.timestamps
+    end
+  end
+end
