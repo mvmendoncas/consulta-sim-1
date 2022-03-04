9035f67c1182b43d0fcaabd37340c49322a9f1c8
rcaa <rodrigo.andrade@ufape.edu.br>
2022-03-04 15:59:33 -0300

    implementacao dos validations de consulta

--- app/models/consulta.rb
+++ app/models/consulta.rb
@@ -1,2 +1,17 @@
 class Consulta < ApplicationRecord
+
+  validate :data_consulta_futuro
+  validate :horarios_validos
+
+  def data_consulta_futuro
+    if data.present? && data < Date.today
+      errors.add(:data, "nao pode ser no passado")
+    end
+  end
+
+  def horarios_validos
+    if %w[08:00 09:00 10:00 11:00 14:00 15:00 16:00 17:00].exclude? horario
+      errors.add(:horario, "horario invalido")
+    end
+  end
 end
