47e8f24b8483e7bd1af59f919b3962af893ca2f3
rcaa <rodrigo.andrade@ufape.edu.br>
2022-03-04 15:58:58 -0300

    implementacao dos validations de endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,2 +1,9 @@
 class Endereco < ApplicationRecord
+
+  validates :logradouro, presence: true, length: {in: 4..30}
+  validates :complemento, length: {in: 2..10}
+  validates_format_of :cep, with: /\A\d{5}-\d{3}\z/, presence: true,
+                      message: 'deve ter formato XXXXX-XXX'
+  validates :bairro, presence: true, length: {in: 2..20}
+  validates :cidade, presence: true, length: {in: 2..20}
 end
