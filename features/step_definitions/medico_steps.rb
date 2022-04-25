cc8934acf7f490efce5bb2f9f6415be197b02f09
rcaa <rodrigo.andrade@ufape.edu.br>
2022-04-25 18:08:47 -0300

    steps de medico criados

--- features/step_definitions/medico_steps.rb
+++ features/step_definitions/medico_steps.rb
@@ -1,15 +1,22 @@
 Given('estou na pagina de medico') do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit 'medicos/new'
+  expect(page).to have_content('New medico')
 end
 
-Given('o medico com CRM {string} existe') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given('o medico com CRM {string} existe') do |crm|
+  fill_in 'medico[nome]', :with => 'Rodrigo Andrade'
+  fill_in 'medico[cpf]', :with => '706.508.980-01'
+  fill_in 'medico[email]', :with => 'rodrigo@rodrigo.com'
+  fill_in 'medico[especialidade]', :with => 'Reumatologista'
+  fill_in 'medico[crm]', :with => crm
+  click_button 'Create Medico'
+  expect(page).to have_content(crm)
 end
 
-When('eu clico em remover o medico com CRM {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When('eu clico em remover o medico com CRM {string}') do |crm|
+  click_button 'Destroy this medico'
 end
 
 Then('eu vejo uma mensagem que o medico foi apagado com sucesso') do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content('Medico was successfully destroyed')
 end
