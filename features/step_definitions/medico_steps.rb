4e13e5d35bae0b9f70e002dd4a2de107239e3099
eemilyy <emilyalmeeidas@gmail.com>
2022-04-27 18:36:01 -0300

    criando novo scenario para medico.feature

--- features/step_definitions/medico_steps.rb
+++ features/step_definitions/medico_steps.rb
@@ -20,3 +20,19 @@ end
 Then('eu vejo uma mensagem que o medico foi apagado com sucesso') do
   expect(page).to have_content('Medico was successfully destroyed')
 end
+
+When('eu preencho os campos de nome {string}, cpf {string}, email {string}, especialidade {string} e crm {string}') do |string, string2, string3, string4, string5|
+  fill_in 'medico[nome]', :with => string
+  fill_in 'medico[cpf]', :with => string2
+  fill_in 'medico[email]', :with => string3
+  fill_in 'medico[especialidade]', :with => string4
+  fill_in 'medico[crm]', :with => string5
+end
+
+When('eu clico em cadastrar novo medico') do
+  click_button 'Create Medico'
+end
+
+Then('eu vejo uma mensagem que o medico foi cadastrado com sucesso') do
+  expect(page).to have_content('Medico was successfully created.')
+end
