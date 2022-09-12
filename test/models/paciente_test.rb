f0fddb1dc2d6d0e7a0ff385b090e20005268b574
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-09-11 23:42:23 -0300

    Testes  no model Paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'criando paciente corretamente' do
+    paciente = Paciente.new nome: 'Joao Jose', cpf: '706.508.980-01', email: 'ricardo@joao.com', data_nascimento: '1990-01-01'
+    assert paciente.save
+  end
+
+  test 'criando paciente sem nome' do
+    paciente = Paciente.new cpf: '706.508.980-01', email: 'karlos@kaio.com', data_nascimento: '1990-01-01'
+    assert_not paciente.save
+  end
+
 end
