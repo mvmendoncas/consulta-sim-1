724e4bbe4dd219ab6344f19172ef4449ebbe0f92
mvmendoncas <mvmendonca.s3301@gmail.com>
2022-09-11 23:42:48 -0300

    Testes no model consulta

--- test/models/consulta_test.rb
+++ test/models/consulta_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ConsultaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'criando consulta corretamente' do
+    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '706.508.980-01', email: 'rodrigo@andrade.com', especialidade: 'Reumatologista', crm: '123456'
+    paciente = Paciente.new nome: 'Joao Jose', cpf: '706.508.980-01', email: 'joao@jose.com' , data_nascimento: '1990-01-01'
+    consulta = Consulta.new data: '2019-01-01', horario: '12:00' , medico: medico, paciente: paciente
+    assert consulta.errors.messages.empty?
+  end
+
+  test 'criando consulta sem data' do
+    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '706.508.980-01', email: 'rodrigo@andrade.com', especialidade: 'Reumatologista', crm: '123456'
+    paciente = Paciente.new nome: 'Joao Jose', cpf: '706.508.980-01', email: 'joao@jose.com' , data_nascimento: '1990-01-01'
+    consulta = Consulta.new horario: '12:00', medico: medico, paciente: paciente
+    assert_not consulta.save
+  end
+
 end
